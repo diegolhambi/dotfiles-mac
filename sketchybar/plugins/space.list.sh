1fedf746c7b748b6aa2675079ba34607a25a66ad
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2022-06-25 19:40:09 -0300

    Removing not used SENDER

--- sketchybar/plugins/space.list.sh
+++ sketchybar/plugins/space.list.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
-update() {
-    args=()
+args=()
 
-    if [ "$NAME" == "template.space.list" ]; then
-        exit 0
-    fi
+if [ "$NAME" == "template.space.list" ]; then
+    exit 0
+fi
 
-    active_spaces=$(yabai -m query --spaces | jq -r 'map(select(."is-visible" == true)) | .[] | .index | @sh')
-    space=$(echo $NAME | cut -d. -f2)
-    if [[ "${active_spaces[*]}" =~ "$space" ]]; then
-        SELECTED=on
-    fi
+active_spaces=$(yabai -m query --spaces | jq -r 'map(select(."is-visible" == true)) | .[] | .index | @sh')
+space=$(echo $NAME | cut -d. -f2)
+if [[ "${active_spaces[*]}" =~ "$space" ]]; then
+    SELECTED=on
+fi
 
-    args+=(--set $NAME label=$NAME icon.highlight=$SELECTED background.drawing=$SELECTED)
+args+=(--set $NAME label=$NAME icon.highlight=$SELECTED background.drawing=$SELECTED)
 
-    sketchybar -m "${args[@]}"
-}
-
-case "$SENDER" in
-    "mouse.entered") sketchybar --set $NAME background.drawing=on #Inactive
-    ;;
-    "mouse.exited") sketchybar --set $NAME background.drawing=off #Inactive
-    ;;
-    *) update
-    ;;
-esac
+sketchybar -m "${args[@]}"
