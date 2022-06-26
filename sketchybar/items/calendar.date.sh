6c26c3d98d0198244b85da06aa5c1f9f973380cf
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2022-06-26 19:00:02 -0300

    Tuning update frequency

--- sketchybar/items/calendar.date.sh
+++ sketchybar/items/calendar.date.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sketchybar --add item calendar.date right                            \
-           --set calendar.date update_freq=10                        \
+           --set calendar.date update_freq=30                        \
                                icon=􀉉                               \
                                click_script="open -a 'Calendar'"     \
                                script="$PLUGIN_DIR/calendar.date.sh"
--- sketchybar/items/sound.input.sh
+++ sketchybar/items/sound.input.sh
@@ -4,6 +4,6 @@ sketchybar -m --add item sound.input right                          \
               --set sound.input                                     \
                     icon=􀊰                                         \
                     label.drawing=off                               \
-                    update_freq=0                                   \
+                    update_freq=2                                   \
                     script="$PLUGIN_DIR/sound.input.sh"             \
                     click_script="$PLUGIN_DIR/sound.input.click.sh"
