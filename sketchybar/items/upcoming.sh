9214a568f3a262ed0c4c8d135b84739e3eb73892
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2023-02-21 18:04:07 -0300

    Simplify upcoming event

--- sketchybar/items/upcoming.sh
+++ sketchybar/items/upcoming.sh
@@ -4,13 +4,7 @@ upcoming=(
     update_freq=20
     updates=on
     label.color=$COLOR_DESACTIVATED_LABEL
-    background.color=$COLOR_BAR
-    popup.height=26
-    popup.blur_radius=25
-    popup.background.corner_radius=4
-    popup.background.color=$COLOR_BAR
     script="python3 $PLUGIN_DIR/upcoming.py"
-    click_script="sketchybar -m --set upcoming popup.drawing=toggle"
 )
 
 sketchybar --add item upcoming right \
