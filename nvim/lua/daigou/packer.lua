bdf26d3960d94951a40c3ee6957a5fe819ffd644
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2023-09-12 20:56:14 -0300

    Use codeium

--- nvim/lua/daigou/packer.lua
+++ nvim/lua/daigou/packer.lua
@@ -39,20 +39,20 @@ return require("packer").startup(function(use)
         branch = "v1.x",
         requires = {
             -- LSP Support
-            { "neovim/nvim-lspconfig" },           -- Required
-            { "williamboman/mason.nvim" },         -- Optional
+            { "neovim/nvim-lspconfig" },             -- Required
+            { "williamboman/mason.nvim" },           -- Optional
             { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { "hrsh7th/nvim-cmp" },       -- Required
-            { "hrsh7th/cmp-nvim-lsp" },   -- Required
-            { "hrsh7th/cmp-buffer" },     -- Optional
-            { "hrsh7th/cmp-path" },       -- Optional
+            { "hrsh7th/nvim-cmp" },         -- Required
+            { "hrsh7th/cmp-nvim-lsp" },     -- Required
+            { "hrsh7th/cmp-buffer" },       -- Optional
+            { "hrsh7th/cmp-path" },         -- Optional
             { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" },   -- Optional
+            { "hrsh7th/cmp-nvim-lua" },     -- Optional
 
             -- Snippets
-            { "L3MON4D3/LuaSnip" },           -- Required
+            { "L3MON4D3/LuaSnip" },             -- Required
             { "rafamadriz/friendly-snippets" }, -- Optional
         }
     })
@@ -74,5 +74,7 @@ return require("packer").startup(function(use)
         requires = { "kyazdani42/nvim-web-devicons", opt = true }
     })
 
-    use("ThePrimeagen/vim-be-good")
+    use('Exafunction/codeium.vim')
+
+    -- use("ThePrimeagen/vim-be-good")
 end)
