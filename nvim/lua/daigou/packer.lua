a2e50fb2714e4e871069ed1d29d8cbb66bb28ece
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2023-11-05 00:01:31 -0300

    Fix telescope

--- nvim/lua/daigou/packer.lua
+++ nvim/lua/daigou/packer.lua
@@ -9,7 +9,7 @@ return require("packer").startup(function(use)
 
     use({
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.1",
+        tag = "0.1.4",
         -- or                            , branch = "0.1.x",
         requires = { { "nvim-lua/plenary.nvim" } }
     })
@@ -32,7 +32,6 @@ return require("packer").startup(function(use)
     use("theprimeagen/harpoon")
     use("mbbill/undotree")
     use("tpope/vim-fugitive")
-    use("nvim-treesitter/nvim-treesitter-context")
 
     use({
         "VonHeikemen/lsp-zero.nvim",
